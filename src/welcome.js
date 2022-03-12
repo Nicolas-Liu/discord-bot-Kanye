c90db20764252f5a56fa251aafb230b517fa7fb0
Nicolas Liu <94934469+Nicolas-Liu@users.noreply.github.com>
2022-03-11 19:06:26 -0500

    Update welcome.js

--- src/welcome.js
+++ src/welcome.js
@@ -1,11 +1,7 @@
 module.exports = client => {
     const serverID = '923081054619844609';
     client.on('guildMemberAdd', (member)=> {
-        console.log(member)
-
-
         const message = `Welcome <@${member.id}> to the kanyeVerse`;
         const channel = member.guild.channels.cache.get(serverID);
-        channel.send(message)
     })
 }
